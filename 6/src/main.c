3df327b5f5e01e12be351477cc156aa181c9ad4e
Mikayla Fischler <micron1314@gmail.com>
2018-12-08 22:07:23 -0500

    working and clean output for day 6

--- 6/src/main.c
+++ 6/src/main.c
@@ -7,8 +7,8 @@
 
 #define INPUT_LENGTH 50
 
-#define URC_ROW 1
-#define URC_COL 2
+#define MAP_PROP_ROW 1
+#define MAP_PROP_COL 2
 
 typedef struct location {
 	uint16_t x;
@@ -16,121 +16,127 @@ typedef struct location {
 	uint32_t area;
 } location_t;
 
-typedef struct unique_rc {
+typedef struct map_prop {
 	uint16_t max_x, min_x;
 	uint16_t max_y, min_y;
-	uint16_t rows[INPUT_LENGTH];
-	uint16_t cols[INPUT_LENGTH];
-} unique_rc_t;
+} map_prop_t;
 
-void _urc_add(unique_rc_t* urc, uint8_t type, uint16_t value) {
-	if (urc) {
-		if (type == URC_ROW) {
-			int i;
-			for (i = 0; i < INPUT_LENGTH; i++) {
-				if (urc->rows[i] == value) { return; }
-				if (!urc->rows[i]) { break; }
-			}
-
-			if (value > urc->max_y) { urc->max_y = value; }
-			if (value < urc->min_y || i == 0) { urc->min_y = value; }
-			urc->rows[i] = value;
+void map_prop_update(map_prop_t* map_prop, uint8_t type, uint16_t value) {
+	if (map_prop) {
+		if (type == MAP_PROP_ROW) {
+			if (value > map_prop->max_y) { map_prop->max_y = value; }
+			if (value < map_prop->min_y || map_prop->min_y == 0) { map_prop->min_y = value; }
 		} else {
-			int i;
-			for (i = 0; i < INPUT_LENGTH; i++) {
-				if (urc->cols[i] == value) { return; }
-				if (!urc->cols[i]) { break; }
-			}
-
-			if (value > urc->max_x) { urc->max_x = value; }
-			if (value < urc->min_x || i == 0) { urc->min_x = value; }
-			urc->cols[i] = value;
+			if (value > map_prop->max_x) { map_prop->max_x = value; }
+			if (value < map_prop->min_x || map_prop->min_x == 0) { map_prop->min_x = value; }
 		}
 	}
-}
-
-void urc_add_row(unique_rc_t* urc, uint16_t y) {
-	_urc_add(urc, URC_ROW, y);
-}
-
-void urc_add_col(unique_rc_t* urc, uint16_t x) {
-	_urc_add(urc, URC_COL, x);
-}
-
-uint8_t _urc_has(unique_rc_t* urc, uint8_t type, uint16_t value) {
-	if (urc) {
-		if (type == URC_ROW) {
-			for (int i = 0; i < INPUT_LENGTH; i++) {
-				if (urc->rows[i] == value) { return 1; }
-				if (!urc->rows[i]) { return 0; }
-			}
-		} else {
-			for (int i = 0; i < INPUT_LENGTH; i++) {
-				if (urc->cols[i] == value) { return 1; }
-				if (!urc->cols[i]) { return 0; }
-			}
-		}
-	}
-	return 0;
-}
-
-uint8_t urc_has_y(unique_rc_t* urc, uint16_t value) {
-	return _urc_has(urc, URC_ROW, value);
-}
-
-uint8_t urc_has_x(unique_rc_t* urc, uint16_t value) {
-	return _urc_has(urc, URC_COL, value);
-}
-
-uint8_t has_inf_area(unique_rc_t* urc, location_t* coord) {
-	return (coord->x >= urc->max_x || coord->x <= urc->min_x || coord->y >= urc->max_y || coord->y <= urc->min_y);
 }
 
 uint8_t is_coord(location_t** locations, uint16_t x, uint16_t y) {
 	for (int i = 0; i < INPUT_LENGTH; i++) {
 		if (locations[i]->x == x && locations[i]->y == y) { return 1; }
 	}
+
 	return 0;
 }
 
-uint16_t distance_to(location_t* loc, uint16_t x, uint16_t y) {
+uint8_t has_inf_area(map_prop_t* map_prop, location_t* coord) {
+	return (coord->x >= map_prop->max_x || coord->x <= map_prop->min_x || coord->y >= map_prop->max_y || coord->y <= map_prop->min_y);
+}
+
+uint8_t adv_has_inf_area(map_prop_t* map_prop, location_t** locations, location_t* coord) {
+	if (has_inf_area(map_prop, coord)) { return 1; }
+
+	uint16_t mid_x = (map_prop->max_x + map_prop->min_x) / 2;
+	uint16_t mid_y = 0;
+	uint16_t crit_distance = 0;
+	uint8_t hits = 0;
+
+	if (coord->x > mid_x) {
+		for (uint16_t x = coord->x; x < map_prop->max_x; x++) {
+			crit_distance = map_prop->max_x - x;
+			if (x <= map_prop->max_x && x >= map_prop->min_x) {
+				for (uint16_t y = (coord->y - crit_distance); y < (coord->y + crit_distance); y++) {
+					if (is_coord(locations, x, y)) { hits++; }
+				}
+			}
+		}
+	} else if (coord->x < mid_x) {
+		for (uint16_t x = map_prop->min_x; x < coord->x; x++) {
+			crit_distance = x - map_prop->min_x;
+			if (x <= map_prop->max_x && x >= map_prop->min_x) {
+				for (uint16_t y = (coord->y - crit_distance); y < (coord->y + crit_distance); y++) {
+					if (is_coord(locations, x, y)) { hits++; }
+				}
+			}
+		}
+	} else if (coord->y > (mid_y = (map_prop->max_y + map_prop->min_y) / 2)) {
+		for (uint16_t y = coord->y; y < map_prop->max_y; y++) {
+			crit_distance = map_prop->max_y - y;
+			if (y <= map_prop->max_y && y >= map_prop->min_y) {
+				for (uint16_t x = (coord->x - crit_distance); x < (coord->x + crit_distance); x++) {
+					if (is_coord(locations, x, y)) { hits++; }
+				}
+			}
+		}
+	} else if (coord->y < mid_y) {
+		for (uint16_t y = map_prop->min_y; y < coord->y; y++) {
+			crit_distance = y - map_prop->min_y;
+			if (y <= map_prop->max_y && y >= map_prop->min_y) {
+				for (uint16_t x = (coord->x - crit_distance); x < (coord->x + crit_distance); x++) {
+					if (is_coord(locations, x, y)) { hits++; }
+				}
+			}
+		}
+	}
+
+	if (hits > 0) { return 1; }
+	return 0;
+}
+
+uint16_t distance_to(location_t* loc, int16_t x, int16_t y) {
 	uint16_t dist_x, dist_y;
+
 	if (loc->x > x) {
 		dist_x = loc->x - x;
 	} else {
 		dist_x = x - loc->x;
 	}
+
 	if (loc->y > y) {
 		dist_y = loc->y - y;
 	} else {
 		dist_y = y - loc->y;
 	}
+
 	return dist_x + dist_y; // abs((int32_t) loc->x - x) + abs((int32_t) loc->y - y);
+}
+
+uint8_t is_less_than_10000(location_t** locations, uint16_t* dists) {
+	uint16_t dist = 0;
+	for (int8_t i = 0; i < INPUT_LENGTH; i++) { dist += dists[i]; }
+	if (dist < 10000) { return 1; }
+	return 0;
 }
 
 void compute_area(location_t** locations, uint16_t* dists) {
 	uint16_t min = 0;
-	int8_t min_idx = 0;
+	uint8_t min_idx = 0;
 	uint8_t num_at_mins = 0;
 
-	for (int8_t i = 0; i < INPUT_LENGTH; i++) {
+	for (uint8_t i = 0; i < INPUT_LENGTH; i++) {
 		if (dists[i] < min || i == 0) {
 			min = dists[i];
 			min_idx = i;
 		}
 	}
 
-	for (int8_t i = 0; i < INPUT_LENGTH; i++) {
-		if (dists[i] == min) {
-			num_at_mins++;
-		}
+	for (uint8_t i = 0; i < INPUT_LENGTH; i++) {
+		if (dists[i] == min) { num_at_mins++; }
 	}
 
-	if (num_at_mins == 1 && dists[min_idx] != 0) {
-		// printf(RED "%lx: %d\n" RESET, (size_t) &locations[min_idx]->area, locations[min_idx]->area);
-		locations[min_idx]->area++;
-		// printf(BLUE "%lx: %d\n" RESET, (size_t) &locations[min_idx]->area, locations[min_idx]->area);
-	}
+	if (num_at_mins == 1) { locations[min_idx]->area++; }
 }
 
 int main(int argc, char** argv) {
@@ -145,7 +151,7 @@ int main(int argc, char** argv) {
 	int num_read = 0;
 
 	// open file
-    fp = fopen("./input.txt.real", "r");
+    fp = fopen("./input.txt", "r");
     if (fp == NULL) { return -1; }
 
 	// timing
@@ -160,7 +166,7 @@ int main(int argc, char** argv) {
 	uint8_t i = 0;
 
 	location_t* locations[INPUT_LENGTH];
-	unique_rc_t* unique_rc = calloc(sizeof(unique_rc_t), 1);
+	map_prop_t* map_prop = calloc(sizeof(map_prop_t), 1);
 
 	printf(YELLOW "starting...\n" RESET);
 
@@ -193,18 +199,18 @@ int main(int argc, char** argv) {
 		location_t* loc = malloc(sizeof(location_t));
 		loc->x = atoi(x_str);
 		loc->y = atoi(y_str);
-		loc->area = 1;
+		loc->area = 0;
 		locations[i++] = loc;
 
-		urc_add_col(unique_rc, loc->x);
-		urc_add_row(unique_rc, loc->y);
-
-		printf("%d, %d\n", loc->x, loc->y);
+		map_prop_update(map_prop, MAP_PROP_COL, loc->x);
+		map_prop_update(map_prop, MAP_PROP_ROW, loc->y);
 	}
 
-	uint16_t map_width = (2000 + 1);
-	uint16_t map_height = (2000 + 1);
+	uint16_t map_width = (map_prop->max_x - map_prop->min_y + 1);
+	uint16_t map_height = (map_prop->max_y - map_prop->min_y + 1);
 	uint16_t*** map = malloc(sizeof(uint16_t**) * map_width);
+	uint32_t region = 0;
+	uint16_t x, y;
 
 	for (int i = 0; i < map_width; i++) {
 		map[i] = malloc(sizeof(uint16_t*) * map_height);
@@ -215,60 +221,50 @@ int main(int argc, char** argv) {
 
 	for (int i = 0; i < map_width; i++) {
 		for (int j = 0; j < map_height; j++) {
-			for (int k = 0; k < INPUT_LENGTH; k++) {
-				if (i == unique_rc->max_x || i == unique_rc->min_x || j == unique_rc->max_y && j == unique_rc->min_y) {
-					map[i][j][k] = -1;
+			x = i + map_prop->min_x;
+			y = j + map_prop->min_y;
 
+			for (int k = 0; k < INPUT_LENGTH; k++) {
+				if (x == map_prop->max_x || x == map_prop->min_x || y == map_prop->max_y && y == map_prop->min_y) {
+					map[i][j][k] = -1;
 				} else {
-				map[i][j][k] = distance_to(locations[k], i, j);
+					map[i][j][k] = distance_to(locations[k], x  , y );
 				}
-				// printf("%lx: %d\n", (size_t) &map[i][j][k], map[i][j][k]);
 			}
 		}
 	}
 
 	for (int i = 0; i < map_width; i++) {
 		for (int j = 0; j < map_height; j++) {
-			if (i < unique_rc->max_x && i > unique_rc->min_x && j < unique_rc->max_y && j > unique_rc->min_y) {
+			x = i + map_prop->min_x;
+			y = j + map_prop->min_y;
+
+			if (x < map_prop->max_x && x > map_prop->min_x && y < map_prop->max_y && y > map_prop->min_y) {
 				compute_area(locations, map[i][j]);
+				region += is_less_than_10000(locations, map[i][j]);
 			}
 		}
 	}
 
 	uint32_t max_area = 0;
 	for (int i = 0; i < INPUT_LENGTH; i++) {
-		printf("area: %d\n", locations[i]->area);
-		if (locations[i]->area > max_area) {
+		if (locations[i]->area > max_area && adv_has_inf_area(map_prop, locations, locations[i])) {
 			max_area = locations[i]->area;
 		}
 	}
-
-	printf("max area: %d\n", max_area);
-
-
-	// // get a set of locations without infinite area
-	// for (int i = 0; i < 50; i++) {
-	// 	if (has_inf_area(unique_rc, locations[i])) {
-	// 		printf("coord %d has infinite area\n", i);
-	// 	} else {
-	// 		// uint32_t area = find_area(locations, locations[i], locations[i]->x, locations[i]->y);
-	// 	}
-	// }
-
-	printf("%d_%d, %d_%d\n", unique_rc->min_x, unique_rc->max_x, unique_rc->min_y, unique_rc->max_y);
 
 	time_end = clock();
 
     printf(GREEN "done.\n" RESET);
 
-	printf(B_WHITE "\ntotal time taken" WHITE "\t: " RED "%f" WHITE " seconds\n" RESET, (double) (time_end - time_start) / CLOCKS_PER_SEC);
-	printf(B_RED "[" MAGENTA "part 1" B_RED "] " B_WHITE "DESC" WHITE "\t\t: " CYAN "VAL\n" RESET);
-	printf(B_RED "[" MAGENTA "part 2" B_RED "] " B_WHITE "DESC" WHITE "\t\t: " CYAN "VAL\n" RESET);
+	printf(B_WHITE "\ntotal time taken" WHITE "\t\t: " RED "%f" WHITE " seconds\n" RESET, (double) (time_end - time_start) / CLOCKS_PER_SEC);
+	printf(B_RED "[" MAGENTA "part 1" B_RED "] " B_WHITE "largest finite area" WHITE "\t: " CYAN "%d\n" RESET, max_area);
+	printf(B_RED "[" MAGENTA "part 2" B_RED "] " B_WHITE "size of region" WHITE "\t\t: " CYAN "%d\n" RESET, region);
 
 	// free up memory
 
-	for (int i = 0; i < (unique_rc->min_x - unique_rc->max_x); i++) {
-		for (int j = 0; j < (unique_rc->min_y - unique_rc->max_y); j++) {
+	for (int i = 0; i < (map_prop->min_x - map_prop->max_x); i++) {
+		for (int j = 0; j < (map_prop->min_y - map_prop->max_y); j++) {
 			free(map[i][j]);
 		}
 		free(map[i]);
